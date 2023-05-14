4fb9801ab9d6aeb2bea4549f4b06b3e30c3e4650
Bruno-Limon <brunolimon@hotmail.com>
2023-05-14 15:37:18 +0200

    update

--- Src/patent-analysis.r
+++ Src/patent-analysis.r
@@ -1,3 +1,15 @@
+library(udpipe)
+library(tidyverse)
+library(ggplot2)
+
+udmodel <- udpipe_download_model(language = "english")
+
+x1 <- udpipe(x = "Competitive intelligence (CI) is the process and forward-looki
+                ng practices used in producing knowledge
+                about the competitive environment to improve organizational
+                performance.", object = udmodel)
+str(x1)
+
 x <- 12
 y <- 10
 z <- x + y
