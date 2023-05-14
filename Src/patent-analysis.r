fce555e7d8a8523cfc9d3b48071f78fdc36cd624
Bruno-Limon <brunolimon@hotmail.com>
2023-05-14 18:05:04 +0200

    update

--- Src/patent-analysis.r
+++ Src/patent-analysis.r
@@ -4,11 +4,12 @@ library(ggplot2)
 
 udmodel <- udpipe_download_model(language = "english")
 
-list_of_files = list.files(path = "C:/Users/Pavilion/Downloads/Pisa/SCI/AI_patents_2020_2021_claim/",
+list_of_files <- list.files(path = "C:/Users/Pavilion/Downloads/Pisa/SCI/
+                                    AI_patents_2020_2021_claim/",
                             pattern = "\\.txt$",
                             full.names = TRUE)
 
 x <- 12
 y <- 10
-z <- x+y
+z <- x + y
 print(z)
