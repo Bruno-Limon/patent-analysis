cfbe13c769a8dddb3f5fdc843f0053c7c8dc2be1
Bruno-Limon <brunolimon@hotmail.com>
2023-05-16 10:54:01 +0200

    update

--- Src/patent-analysis.r
+++ Src/patent-analysis.r
@@ -8,37 +8,45 @@ list_patents <- list.files(path = "C:/Users/Pavilion/Desktop/Github/patent-analy
 
 #set how many patents we want to read (for time purposes)
 n_patents <- 10
+col_names <- c("id", "filing_date", "publication_date", "ipc_classes",
+               "assignee", "inventors", "title", "abstract", "claims")
+
 # Create an empty tibble, that will store all the information
-patents <- tibble(id = rep("", n_patents),
-                  IPC = rep("", n_patents),
-                  title = rep("", n_patents),
-                  abstract = rep("", n_patents),
-                  claims = rep("", n_patents))
+patents <- data.frame(matrix(nrow = n_patents, ncol = length(col_names)))
+colnames(patents) <- col_names
 
 for (i in 1:n_patents){
     raw_text <- read_file(list_patents[[i]])
     # id
     patents[[i, 1]] <- str_remove_all(string = list_patents[[i]],
                                       pattern = "C:/Users/Pavilion/Desktop/Github/patent-analysis/Data/Patents/|.txt")
-    # IPC
-    patents[[i, 2]] <- str_extract(string = raw_text,
-                                   pattern = "<ipc_classes>\n(.*?)\n</ipc_classes>") %>%
-    str_remove_all("<ipc_classes>\n|\n</ipc_classes>")
 
-    # title
-    patents[[i, 3]] <- str_extract(string = raw_text,
-                                   pattern = "<title>\n(.*?)\n</title>") %>%
-    str_remove_all("<title>\n|\n</title>")
-
-    # abstract
-    patents[[i, 4]] <- str_extract(string = raw_text,
-                                   pattern = "<abstract>\n(.*?)\n</abstract>") %>%
-    str_remove_all("<abstract>\n|\n</abstract>")
-
-    # claims
-    # patents[[i, 5]] <- str_extract(string = raw_text,
-    #                                pattern = "<claims>\n(.*?)\n</claims>") %>%
-    # str_remove_all("<claims>\n|\n</claims>")
+    for (j in 2:length(col_names)){
+        patents[[i, j]] <- str_extract(string = raw_text,
+                                       pattern = paste0("<", col_names[j],
+                                                        ">\n(.*?)\n</",
+                                                        col_names[j], ">")) %>%
+        str_remove_all(paste0("<", col_names[j], ">\n|\n</", col_names[j], ">"))
     }
+}
 
-head(patents)
+# ud_model <- udpipe_download_model(language = "english")
+# ud_model <- udpipe_load_model(ud_model$file_model)
+# patents_tagged <- udpipe_annotate(ud_model,
+#                                   x = patents$abstract,
+#                                   doc_id = patents$id) %>%
+# as_tibble()
+
+# patents_tagged %>%
+# count(upos) %>%
+# ggplot(aes(x = reorder(upos, n), y = n)) +
+# geom_bar(stat = "identity") +
+# coord_flip()
+
+# patents_tagged %>%
+# filter(upos == "NOUN") %>%
+# count(token) %>%
+# filter(n > 25) %>%
+# ggplot(aes(x = reorder(token, n), y = n)) +
+# geom_bar(stat = "identity") +
+# coord_flip()
